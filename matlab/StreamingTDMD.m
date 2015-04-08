67bd17a925d9de413b0bb3c1a33221d5ec4d3a0c
Maziar Hemati <mhemati@nat-oitwireless-inside-vapornet100-c-15110.princeton.edu>
2015-04-08 08:04:37 -0400

    Add Matlab implementation of streaming TDMD. This code implements the noise-robust total-least-squares formulation of DMD, as described in Hemati and Rowley (2015), using the streaming DMD framework presented in Hemati, Williams, and Rowley (2014).

---
+++ matlab/StreamingTDMD.m
@@ -0,0 +1,156 @@
+%STREAMINGTDMD  incrementally updated TLS Dynamic Mode Decomposition
+% Usage:
+%   stdmd = StreamingTDMD
+%   stdmd = StreamingTDMD(r)
+%
+%   r: the maximum dimension of the subspace on which DMD modes are
+%      computed, using a POD-truncation version of the algorithm.
+%      If r=0 (the default), use the direct algorithm, equivalent
+%      to a batch-processed DMD.
+%
+%   A StreamingTDMD object has two methods:
+%     update         adds a pair of snapshots to the dataset
+%     compute_modes  computes the DMD modes and eigenvalues
+%
+%   For details, see
+%       M.S. Hemati, M.O. Williams, C.W. Rowley,
+%       ``Dynamic Mode Decomposition for Large and Streaming Datasets,''
+%       Physics of Fluids, vol. 26, 111701 (2014).
+%
+%       M.S. Hemati, C.W. Rowley,
+%       ``De-Biasing the Dynamic Mode Decomposition for Applied Koopman 
+%       Spectral Analysis,'' submitted to PNAS (2015).
+%
+% Tested using Matlab 8.1.0.604 (R2013a) for Mac OSX.
+%
+% Authors:
+%   Maziar S. Hemati
+%   Matthew O. Williams
+%   Clarence W. Rowley
+%
+% Created:
+%   March 2015.
+%
+%   See also: StreamingTDMD/UPDATE, StreamingTDMD/COMPUTE_MODES, sdmd_run
+%
+% Reference page in Help browser:
+%   <a href="matlab:doc StreamingTDMD">doc StreamingTDMD</a>
+
+classdef StreamingTDMD
+    properties
+        count = 0;    % number of pairs of snapshots processed
+        max_rank = 0; % maximum allowable rank of DMD operator
+        Qz;
+        Gz;
+    end
+
+    methods
+        function obj = StreamingTDMD(max_rank)
+            % StreamingDMD  Create an object for incrementaly updated DMD.
+            %   sdmd = StreamingDMD  or
+            %   sdmd = StreamingDMD(0)  Use the direct algorithm
+            %        (equivalent to batch-processed DMD)
+            %
+            %   obj = StreamingDMD(max_rank)  Use a POD-truncation
+            %     version of the algorithm, with max_rank corresponding
+            %     to the truncation threshold (i.e., the maximum number
+            %     of POD modes retained).
+
+            if nargin == 1
+                obj.max_rank = max_rank;
+            end
+        end
+
+        function obj = update(obj, xin, yin)
+            % UPDATE  Update the DMD computation with a pair of snapshots
+            %
+            %   sdmd = sdmd.UPDATE(x,y) adds the pair of snapshots (x,y)
+            %     to the data ensemble.  Here, if the (discrete-time)
+            %     dynamics are given by z(n+1) = f(z(n)), then (x,y)
+            %     should be measurements correponding to consecutive states
+            %     z(n) and z(n+1).
+
+            % parameters
+            ngram = 5;      % number of times to reapply Gram-Schmidt
+            epsilon = eps;  % tolerance for expanding the bases
+
+            obj.count = obj.count + 1;
+
+            % construct augmented snapshot
+            zin = [xin;yin];
+            normz = norm(zin);
+            n = size(xin,1);
+            
+            
+            % ---- Process the First Iterate ----
+            if obj.count == 1
+                % construct bases
+                obj.Qz = zin/norm(zin);                
+                z = obj.Qz*obj.Qz'*zin;
+                obj.Gz = norm(z)^2; 
+                return
+            end
+            
+            % ---- TLS Stage, Step 1 ----
+            % classical Gram-Schmidt reorthonormalization
+            ztilde = zeros(size(obj.Qz,2),1);
+            ez = zin;
+            for igram = 1:ngram
+                dz = obj.Qz' * ez;
+                ztilde = ztilde + dz;
+                ez = ez - obj.Qz * dz;
+            end
+            
+            % ---- TLS Stage, Step 2 ----
+            % check basis for z and expand, if necessary
+            if norm(ez) / normz > epsilon
+                % update basis for z
+                obj.Qz = [obj.Qz ez/norm(ez)];
+                % increase size of Gz (by zero-padding)
+                obj.Gz = [obj.Gz zeros(size(obj.Gz,1),1); zeros(1,size(obj.Gz,2)+1)];
+            end
+            
+            
+            ztilde = obj.Qz'*zin;
+            obj.Gz = obj.Gz + ztilde*ztilde';
+            
+            
+            % ---- TLS Stage, Step 3 ----
+            % check if POD compression is needed
+            
+            r0 = obj.max_rank;
+            
+            if r0
+                if size(obj.Qz,2) > r0
+                    [evec, eval] = eig(obj.Gz);
+                    [eval, indx] = sort(diag(eval),'descend');
+                    qz = evec(:,indx(1:r0));
+                    obj.Qz = obj.Qz * qz;
+                    obj.Gz = diag(eval(1:r0));
+                end
+            end
+            
+        end
+
+        function [modes, evals] = compute_modes(obj)
+            % COMPUTE_MODES  Compute DMD modes and eigenvalues
+            %
+            % [V, lam] = sdmd.COMPUTE_MODES produces a vector lam
+            %   of DMD eigenvalues, and a matrix V whose columns are
+            %   DMD modes of the current dataset.
+
+            n = size(obj.Qz, 1)/2;
+            [Qx, Rx] = qr(obj.Qz(1:n, :));
+            r0 = obj.max_rank;
+            Qx = Qx(:,1:r0);
+            Rx = Rx(1:r0,:);
+            Gx = Rx*obj.Gz*Rx';
+            A = Qx'*obj.Qz(n+1:end, :)*obj.Gz*obj.Qz(1:n, :).'*Qx;
+            
+            Ktilde = A * pinv(Gx);
+            [evecK, evals] = eig(Ktilde);
+            evals = diag(evals);
+            modes = Qx * evecK;
+        end
+    end
+end
---
+++ matlab/stdmd_run.m
@@ -0,0 +1,163 @@
+%STDMD_RUN An example to demonstrate incrementally updated DMD.
+%
+%   Run a simple example of incrementally updated TDMD on a toy problem to
+%   demonstrate the streaming TDMD algorithm, as described in:
+%   
+%       M.S. Hemati, M.O. Williams, C.W. Rowley,
+%       ``Dynamic Mode Decomposition for Large and Streaming Datasets,''
+%       Physics of Fluids, vol. 26, 111701 (2014).
+%
+%       M.S. Hemati, C.W. Rowley,
+%       ``De-Biasing the Dynamic Mode Decomposition for Applied Koopman 
+%       Spectral Analysis,'' submitted to PNAS (2015).
+%
+%   The example considers an arbitrary n-dimensional dynamical system with 
+%   two characteristic frequencies.
+%
+%   A total of m snapshot pairs are measured sequentially and are subject 
+%   to additive i.i.d. zero-mean Gaussian noise with covariance noise_cov.
+%
+%   To toggle between streaming and batch-processed DMD modes, toggle the
+%   streaming flag between true and false, respectively.  The StreamingDMD 
+%   Matlab class is used to perform incremental updates to the DMD 
+%   computation when streaming=true.
+%
+%   When in streaming mode, to run the direct streaming algorithm set 
+%   max_rank=0; otherwise, incrementally update DMD with POD truncation,
+%   where max_rank denotes the truncation threshold (i.e., the maximum
+%   number of retained POD modes).
+%
+% Tested using Matlab 8.1.0.604 (R2013a) for Mac OSX.
+%
+% Authors:
+%   Maziar S. Hemati
+%   Matthew O. Williams
+%   Clarence W. Rowley
+%
+% Created:
+%   August 2014.
+%
+%   see also StreamingTDMD
+%
+% Reference page in Help browser:
+%   <a href="matlab:doc stdmd_run">doc stdmd_run</a>
+
+
+% set example parameters here
+max_rank =10;      % maximum allowable rank of the DMD operator
+                    %   (set to zero for unlimited)
+m = 1001;            % total number of snapshots to be processed
+n = 4001;           % number of states
+noise_cov = 1e-1;   % measurement noise covariance
+rng(0)              % seed the random number generator
+streaming = true;   % true=use streaming DMD, false=use batch-processed DMD
+
+col = 'ms';
+%% define the example system
+% random state directions
+v1 = randn(n,1);
+v2 = randn(n,1);
+v3 = randn(n,1);
+v4 = randn(n,1);
+
+% characteristic frequencies
+f1 = 5.2;
+f2 = 1.0;
+
+% sampling time
+dt = 1e-2;
+
+dynsys = @(k) v1*cos(2*pi*dt*f1*k) + v2*cos(2*pi*f2*dt*k) + ...
+    v3*sin(2*pi*dt*f1*k) + v4*sin(2*pi*f2*dt*k);
+
+get_snapshot = @(k) dynsys(k) + sqrt(noise_cov) * randn(n,1);
+
+%% collect the data
+
+disp('Collecting data')
+tic
+if ~streaming
+    % standard algorithm: batch-processing
+    X = zeros(n, m);
+    Y = zeros(n, m);
+    yk = get_snapshot(0);
+    for k = 1:m
+        xk = yk;
+        yk = get_snapshot(k);
+        X(:,k) = xk;
+        Y(:,k) = yk;
+    end
+    [Qx, S, V] = svd(X, 0);
+    Ktilde = Qx' * Y * V * pinv(S);
+else
+    % streaming algorithm    
+    stdmd = StreamingTDMD(max_rank);
+    sdmd = StreamingDMD(max_rank);
+    
+    yk = get_snapshot(0);
+    for k = 1:m
+        xk = yk;
+        yk = get_snapshot(k);
+        stdmd = stdmd.update(xk, yk);
+        sdmd = sdmd.update(xk, yk);
+    end
+end
+elapsed_time = toc;
+fprintf('  Elapsed time: %f seconds\n', elapsed_time)
+
+
+%% compute DMD spectrum
+disp('Computing spectrum')
+tic
+if ~streaming
+    % standard DMD spectrum
+    [evecK, evals] = eig(Ktilde);
+    evals = diag(evals);
+    modes = Qx * evecK;
+else
+    % streaming algorithm
+    [modestls, evalstls] = stdmd.compute_modes();
+    [modes, evals] = sdmd.compute_modes();
+end
+elapsed_time = toc;
+fprintf('  Elapsed time: %f seconds\n', elapsed_time)
+
+% calculate corresponding frequencies
+fdmd = abs(angle(evals)) ./ (2 * pi * dt);
+ydmd = zeros(length(fdmd),1);
+for ii = 1:length(fdmd)
+    ydmd(ii) = norm(modes(:,ii)).*abs(evals(ii));
+end
+ydmd = ydmd./max(ydmd);
+
+fdmdtls = abs(angle(evalstls)) ./ (2 * pi * dt);
+ydmdtls = zeros(length(fdmdtls),1);
+for ii = 1:length(fdmdtls)
+    ydmdtls(ii) = norm(modestls(:,ii)).*abs(evalstls(ii));
+end
+ydmdtls = ydmdtls./max(abs(ydmdtls));
+
+figure(1)
+stem(fdmd,ydmd,'bo-')
+hold on;
+stem(fdmdtls,ydmdtls,'ro--')
+xlabel('Frequency')
+ylabel('Magnitude')
+
+figure(2);hold on;
+plot(real(evals),imag(evals),'bo',real(evalstls),imag(evalstls),col);
+hold on;
+th = [0:.01:2*pi];
+plot(cos(th),sin(th),'k--'); axis equal;
+
+% % normalize modes to unit norm
+% for ii = 1:length(fdmdtls)
+%     normmodes(:,ii) = modestls(:,ii)./norm(modestls(:,ii));
+% end
+% 
+% % compute mode energies based on the last snapshot
+% enrg = normmodes\yk;
+% 
+% % plot mode energies
+% figure(3);hold on;
+% plot(abs(enrg),col)
